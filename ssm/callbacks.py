b18fbb926b39ef0f8f267e0dd2e35da4a8c7ea94
Fabio Bonassi <bonassi.fabio.94@gmail.com>
2024-12-10 16:00:22 +0100

    Fixed typo in license header

--- ssm/callbacks.py
+++ ssm/callbacks.py
@@ -14,7 +14,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU Lesser General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with gddpc.  If not, see <http://www.gnu.org/licenses/>.
+along with ssm.  If not, see <http://www.gnu.org/licenses/>.
 '''
 
 import re
